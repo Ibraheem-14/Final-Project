70913ad194e3878eda5dd005b77f99ec96721a22
Ibraheem-14 <ibraheem-hussain@outlook.com>
2025-01-16 19:11:22 +0000

    altered the way the data was tested to be empty

--- tests/test_data_loading.py
+++ tests/test_data_loading.py
@@ -3,11 +3,12 @@ import pandas as pd
 
 def test_FileRead():
     ev_df = pd.read_csv('Electric_Vehicle_Population_Data.csv')
-    assert ev_df is not None,"Dataset has not been read"
+    assert not ev_df.empty,"Dataset has not been read"
 
     
 def test_isEmpty():
     ev_df = pd.read_csv('Electric_Vehicle_Population_Data.csv')
     assert len(ev_df) > 0,"Dataset has no data"
+
 
 
